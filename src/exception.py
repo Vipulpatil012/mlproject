9bb18b21efea9001b62b946455298c3c7d760f4f
Vipulpatil012 <Vipulpatil012@gmail.com>
2023-05-17 19:23:34 +0530

    logging and exception

--- src/exception.py
+++ src/exception.py
@@ -22,11 +22,5 @@ class CustomException(Exception):
     
     
     
-if __name__=="__main__":
-        
-        try:
-            a=1/0
-        except Exception as e:
-            logging.info("Divided by Zero")
-            raise CustomException(e,sys)
+
                             
